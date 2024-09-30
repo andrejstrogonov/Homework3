466ad53510e08cdd90953a9a96874b69d0617257
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-30 12:30:55 +0300

    task4 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int performanceOfMachine = 16/2;
+        int twentyMinutes = 20;
+        int twentyMinutePerformance = performanceOfMachine*20;
+        System.out.println("За 20 минут машина произвела "+ twentyMinutePerformance + " штук бутылок");
+        int day = 60*24;
+        int dayPerformance = performanceOfMachine*60*24;
+        System.out.println("За сутки машина произвела "+ dayPerformance + " штук бутылок");
+        int threeDays = 3*60*24;
+        int threeDaysPerformance = dayPerformance*3;
+        System.out.println("За 3 дня машина произвела "+ threeDaysPerformance + " штук бутылок");
+        int month = 30*day;
+        int monthPerformance = dayPerformance*30;
+        System.out.println("За месяц машина произвела "+ monthPerformance + " штук бутылок");
     }
 }
