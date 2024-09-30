d52deb6f786a41af98679f6ec71d95efe89bee8f
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-30 14:58:42 +0300

    task7 completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int averageOfWeightKg = 7;
+        int averageGrammsToKg = 1000;
+        int firstStrategyG = 250;
+        int secondStrategyG = 500;
 
-
+        int firstStrategy = (averageGrammsToKg*averageOfWeightKg)/firstStrategyG;
+        System.out.println("Дней по стратегии 250 г за день: "+firstStrategy);
+        int secondStrategy = (averageGrammsToKg*averageOfWeightKg)/secondStrategyG;
+        System.out.println("Дней по стратегии 500 г за день: "+secondStrategy);
     }
 }
