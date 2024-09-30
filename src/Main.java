c11a9b81e119eccf77d59cc85221c19bad5d09d6
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-30 11:33:03 +0300

    task2 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float firstVariable = 27.12f;
+        long secondVariable = 987678965549L;
+        float thirdVariable = 2.786f;
+        short forthVariable = 569;
+        short fifthVariable = -159;
+        short sixthVariable = 27897;
+        byte seventhVariable = 67;
     }
 }
