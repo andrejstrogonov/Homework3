97e911879b237888674bcbd24d1786d9cb4eef46
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-30 11:50:09 +0300

    task3 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte lessonLyudmyla = 23;
+        byte lessonAnna = 27;
+        byte lessonEkaterina = 30;
+        short sumOfSheets = 480;
+        int sumOfLessons = lessonAnna+lessonEkaterina+lessonLyudmyla;
+        int result = sumOfSheets / sumOfLessons;
+        System.out.println("На каждого ученика рассчитано "+result+" листов бумаги");
     }
 }
