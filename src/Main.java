8b31b854be1f9662dc39527ad61c068ca80760f9
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-30 15:47:49 +0300

    task1 corrections

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int firstVariable = 41000;
+        System.out.println("Значение переменной firstVariable c типом int равно "+firstVariable);
         byte secondVariable = 125;
+        System.out.println("Значение переменной secondVariable c типом byte равно "+secondVariable);
         short thirdVariable = 30000;
+        System.out.println("Значение переменной thirdVariable c типом short равно "+thirdVariable);
         long forthVariable = 985636366L;
+        System.out.println("Значение переменной forthVariable c типом long равно "+forthVariable);
         float piVariable = 3.14f;
+        System.out.println("Значение переменной piVariable c типом float равно "+piVariable);
         double eulerVariable = 1.7d;
+        System.out.println("Значение переменной eulerVariable c типом double равно "+eulerVariable);
     }
 }
