689de9830b2c579f575dbb9fffe937718972c496
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-30 14:46:00 +0300

    task6 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int countsOfBananas = 5;
+        int weightOfBanana = 80;
+        int sumWieghtOfBananas = countsOfBananas*weightOfBanana;
+
+        int countsOfIceCream = 2;
+        int weightOfIceCream = 100;
+        int sumWeightOfIceCream = countsOfIceCream*weightOfIceCream;
+
+        int countsOfEggs = 4;
+        int weightOfEgg = 70;
+        int sumWeightOfEggs = countsOfEggs*weightOfEgg;
+
+        int weightOfMilk = 200;
+        int oneOfOneMilk = 100;
+        int weightOfMilkMl = 105;
+       float weightPerLiterOfMilk = (oneOfOneMilk/weightOfMilkMl)*weightOfMilk;
+
+       float sumOfWeights = sumWieghtOfBananas+sumWeightOfIceCream+sumWeightOfEggs+weightPerLiterOfMilk;
+       System.out.println("Общий вес в граммах равен: "+ sumOfWeights);
+       float sumOfWeightsKg = sumOfWeights/1000;
+       System.out.println("Общий вес в килограммах равен: "+ sumOfWeightsKg);
+
+
     }
 }
